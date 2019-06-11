dc55a6760ef15418faa23d04d599dc360f9ebe67
Mostafa Poorhosseini <48451083+MPComputerChi@users.noreply.github.com>
2019-06-11 13:28:58 -0700

    Update logo.py

--- logo.py
+++ logo.py
@@ -7,16 +7,8 @@ def print_logo(self):
     x = """
 
 
-                Mostafa Poorhosseini
-                    _        ______            _       _ _
-                                                                                     
-    _/      _/                _/_/                                             
-   _/_/    _/              _/    _/    _/    _/    _/_/      _/_/    _/_/_/    
-  _/  _/  _/  _/_/_/_/_/  _/  _/_/    _/    _/  _/_/_/_/  _/_/_/_/  _/    _/   
- _/    _/_/              _/    _/    _/    _/  _/        _/        _/    _/    
-_/      _/                _/_/  _/    _/_/_/    _/_/_/    _/_/_/  _/    _/     
-
- Note! :this script for calculate time .       
+   
+       
 """
     for N, line in enumerate(x.split("\n")):
         sys.stdout.write("\x1b[1;%dm%s%s\n" % (random.choice(colors), line, clear))
